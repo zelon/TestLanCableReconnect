1495ec0b9fe9b4ba4a3dfa112746e3b8dc93b67e
KIM, JINWOOK <zelonion@gmail.com>
2022-08-15 01:15:37 +0900

    Initial Commit

---
+++ TestClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+
+void WriteLine(string msg) => Console.WriteLine(msg);
+
+string hostname = "localhost";
+if (args.Length > 0)
+{
+    hostname = args[0];
+}
+int port = 8080;
+WriteLine($"Connecting to {hostname}:{port}");
+TcpClient tcpClient = new TcpClient(hostname, port);
+WriteLine($"Connected");
+
+byte[] buffer = new byte[1024];
+int index = 0;
+try
+{
+    while (true)
+    {
+        string content = $"{index}";
+        WriteLine($"Sending {content}");
+        buffer = System.Text.Encoding.UTF8.GetBytes(content);
+        tcpClient.GetStream().Write(buffer, 0, buffer.Length);
+        System.Threading.Thread.Sleep(1000);
+        ++index;
+    }
+}
+catch (SocketException socketException)
+{
+    WriteLine($"SocketException Occurred,exception:{socketException.Message}");
+}
+
+WriteLine("End of send");
---
+++ TestServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+
+void WriteLine(string msg) => Console.WriteLine(msg);
+
+int port = 8080;
+TcpListener listener = new TcpListener(System.Net.IPAddress.Any, port);
+WriteLine($"Start port:{port}");
+listener.Start();
+WriteLine($"Started");
+
+var socket = listener.AcceptSocket();
+WriteLine("Socket accepted");
+byte[] buffer = new byte[1024];
+var receivedBytes = socket.Receive(buffer);
+try
+{
+    while (receivedBytes > 0)
+    {
+        string content = System.Text.Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+        Console.WriteLine($"receivedBytes:{receivedBytes},content:{content}");
+        receivedBytes = socket.Receive(buffer);
+    }
+}
+catch (System.Net.Sockets.SocketException socketException)
+{
+    WriteLine($"SocketException Occurred,exception:{socketException.Message}");
+}
+WriteLine("End of program");
